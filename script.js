532796be7ad31c494c0bdba3296a11605235bae3
Julianna Vaccaro <julianna.vicente@icloud.com>
2026-07-18 15:01:36 -0400

    Added more styling in css

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const { createElement } = require("react");
+
 const cats = [
     {
         name: "Pepper",
@@ -5,15 +7,27 @@ const cats = [
         age: 1,
         adopted: false
     }
-    {
+{
         name: "Big Boy",
         breed: "Domestic Short Hair",
         age:  4,
         adopted: false
     }
+
 ];
 
 
 const catContainer = document.getElementById("catContainer");
 
 const catForm = document.querySelector("#catForm");
+
+function displayCats (){
+    for(let i = 0; i < cats.length; i++){
+        
+        const cat = cats[i];
+    }
+    const catInfo = document.createElement("div");
+    const catName = document.createElement("uhhh... something");
+    
+
+}
